293658dc951430904b06c927d1c8de0f00e68bd2
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2022-01-27 03:06:40 +0300

    binary tree search2

--- binary-search-trees/binary.search.tree.js
+++ binary-search-trees/binary.search.tree.js
@@ -82,18 +82,93 @@ class BinarySearchTree {
       ? this.find(val, node.left)
       : this.find(val, node.right);
   }
+
+  bfs() {
+    let data = [];
+    let node = this.root;
+    let queue = [];
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+  //DFS
+  preOrder() {
+    let data = [];
+    let current = this.root;
+    const traverse = (node) => {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+    traverse(current);
+    return data;
+  }
+
+  postOrder() {
+    let data = [];
+    let current = this.root;
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    };
+    traverse(current);
+    return data;
+  }
+  inOrder() {
+    let data = [];
+    let current = this.root;
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    };
+    traverse(current);
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
-tree.insertBT(10);
-tree.insertBT(2);
-tree.insertBT(10);
-tree.insertBT(15);
-tree.insertBT(120);
-tree.insertBT(10);
-tree.insertBT(101);
+// tree.insertBT(10);
+// tree.insertBT(2);
+// tree.insertBT(10);
+// tree.insertBT(15);
+// tree.insertBT(120);
+// tree.insertBT(45);
+// tree.insertBT(101);
+tree.insertBT('a');
+tree.insertBT('d');
+tree.insertBT('b');
+tree.insertBT('e');
+tree.insertBT('f');
+tree.insertBT('c');
+tree.insertBT('g');
 
-console.log(tree.find(101));
-console.log(tree.find(1010));
+// console.log(tree.find(101));
+// console.log(tree.find(1010));
 
 // console.log(tree);
+
+// Traversing
+
+// Breadth First Search
+//
+
+// Depth First Search
+// In-Order Traversal
+// Left -> Root -> Right
+
+// Pre-order Traversal
+// Root -> Left -> Right
+
+// Post-order Traversal
+// Left -> Right -> Root
+// console.log(tree.bfs());
+console.log('Pre-order -> ', tree.preOrder());
+console.log('Post-order -> ', tree.postOrder());
+console.log('In-order -> ', tree.inOrder());
